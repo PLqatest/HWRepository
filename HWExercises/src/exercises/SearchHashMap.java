4916f87dc73160b48ea924a81f4611ee640d977a
madhu <madhu@Pitchuka.attlocal.net>
2017-06-28 14:15:44 -0400

    Committed searching a key using Hash Map

---
+++ HWExercises/src/exercises/SearchHashMap.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchHashMap {
+
+	static void HMProg(){
+			
+			HashMap<Integer,String> HMP = new HashMap <Integer,String> ();
+			
+			HMP.put(1, "one");
+			HMP.put(2, "two");
+			HMP.put(3, "three");
+			HMP.put(4, "four");
+			HMP.put(5, "five");
+			
+			for(int i=1;i<=HMP.size();i++)
+				System.out.println(HMP.get(i));		
+			System.out.println("********");
+			
+		//  To search for a key in the created map and then returns its value
+			
+			for(Map.Entry<Integer, String> entry : HMP.entrySet())
+			{			
+				int key = entry.getKey();
+				String value = entry.getValue();
+				System.out.println(key+" : "+value);
+			}
+			
+		}
+		
+		public static void main(String[] args) {
+			
+			SearchHashMap.HMProg();
+
+		}
+
+	}	
